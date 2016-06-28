e391648b92ec4bde8d27220170b715ed2143145f
Mauricio Eduardo <olive.y.ra@hotmail.com>
2016-06-28 09:12:06 -0300

    Add styles

---
+++ src/main/java/br/com/mauricioeduardo/giflib/controller/GifController.java
@@ -0,0 +1,13 @@
+package br.com.mauricioeduardo.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class GifController {
+        @RequestMapping("/")
+        public String listGifs(){
+            return "home";
+        }
+}
